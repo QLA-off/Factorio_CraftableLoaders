9567236e4d8c43642945c249f268718f5376598b
QLA-off <lenesley.kylian@gmail.com>
2026-08-18 22:52:16 +0200

    v1.1.1

--- data-updates.lua
+++ data-updates.lua
@@ -2,19 +2,31 @@
 if data.raw.recipe["loader"] then
     data.raw.recipe["loader"].enabled = false
     data.raw.recipe["loader"].hidden = false
+
+    -- unhide the loader item as well so it can be used in the "cursor ghost" panel
+    data.raw.item["loader"].hidden = false
 end
 
 if data.raw.recipe["fast-loader"] then
     data.raw.recipe["fast-loader"].enabled = false
     data.raw.recipe["fast-loader"].hidden = false
+
+    -- unhide the loader item as well so it can be used in the "cursor ghost" panel
+    data.raw.item["fast-loader"].hidden = false
 end
 
 if data.raw.recipe["express-loader"] then
     data.raw.recipe["express-loader"].enabled = false
     data.raw.recipe["express-loader"].hidden = false
+
+    -- unhide the loader item as well so it can be used in the "cursor ghost" panel
+    data.raw.item["express-loader"].hidden = false
 end
 
 if data.raw.recipe["turbo-loader"] then
     data.raw.recipe["turbo-loader"].enabled = false
     data.raw.recipe["turbo-loader"].hidden = false
+
+    -- unhide the loader item as well so it can be used in the "cursor ghost" panel
+    data.raw.item["turbo-loader"].hidden = false
 end
